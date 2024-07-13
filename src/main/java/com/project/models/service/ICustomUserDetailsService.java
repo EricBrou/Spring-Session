886078242a6fb830dc2b677807773ed68893b712
EricBrou <eric.renan_keyboard@hotmail.com>
2024-07-13 19:31:15 -0300

    Pequenas alterações e definição das rotas permitidas no spring security

--- src/main/java/com/project/models/service/ICustomUserDetailsService.java
+++
@@ -1,5 +0,0 @@
-package com.project.models.service;
-
-public interface ICustomUserDetailsService {
-
-}
--- src/main/java/com/project/models/service/impl/CartService.java
+++ src/main/java/com/project/models/service/impl/CartService.java
@@ -1,7 +1,6 @@
 package com.project.models.service.impl;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
